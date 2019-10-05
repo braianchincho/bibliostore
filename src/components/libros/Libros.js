c52c1396ed27e4e71b9e9ee3953625067f60a274
braianchincho <braianchincho@gmail.com>
2019-10-05 13:59:41 -0300

    libros

--- src/components/libros/Libros.js
+++ src/components/libros/Libros.js
@@ -1,9 +1,85 @@
-import React from 'react';
-
-const Libros = () => {
+import React, { Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/spinner';
+import PropTypes from 'prop-types';
+const Libros = ({libros}) => {
+    if(!libros) return <Spinner/>;
     return (  
-        <h1>Libros</h1>
+        <div className="row">
+            <div className="col-12 mb-4">
+                <Link to="/libros/nuevo"
+                 className="btn btn-primary"
+                >
+                    <i className="fas fa-plus"></i>{' '}
+                    Nuevo Libro
+                </Link>
+            </div>
+            <div className="col-md-8">
+                <h2>
+                    <i className="fas fa-book"></i>{' '}
+                    Libros
+                </h2>
+            </div>
+            <table className="table table-striped mt-4">
+                <thead className="text-white bg-primary">
+                    <tr>
+                        <th>Titulo</th>
+                        <th>ISBN</th>
+                        <th>Editorial</th>
+                        <th>Existencia</th>
+                        <th>Disponibles</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        libros.map(libro => (
+                            <tr key={libro.id}>
+                                <td>{libro.titulo}</td>
+                                <td>{libro.ISBN}</td>
+                                <td>{libro.editorial}</td>
+                                <td>{libro.existencia}</td>
+                                <td>{libro.existencia 
+                                    - libro.prestados.length}</td>
+                                <td>
+                                    <Link
+                                        to={`/libros/mostrar/${libro.id}`}
+                                        className="btn btn-success btn-block"
+                                    >
+                                    <i className="fas fa-angle-double-right"></i>{' '}
+                                    Más Información
+                                    </Link>
+                                    <button
+                                        type="button"
+                                        className="btn btn-danger btn-block"
+                                    >
+                                        <i className="fas fa-trash-alt"></i>{' '}
+                                        Eliminar
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+
+        </div>
     );
 }
  
-export default Libros;
+Libros.protoTypes = {
+    firestore: PropTypes.object.isRequired,
+    libros: PropTypes.array
+}
+
+export default compose(
+    firestoreConnect([{collection: 'libros'}]),
+    connect((state,props) => {
+        return {
+            libros: state.firestore.ordered.libros
+        }
+    })
+)(Libros);
